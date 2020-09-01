7e996527a8039aa9f3bffc949e7658a99af4348c
Emily Phee <ephee@uci.edu>
2020-09-01 16:06:16 -0700

    changed class syntax on hangmnan.js

--- hangman.js
+++ hangman.js
@@ -1,78 +1,75 @@
-const Hangman = function(word, guessesAllowed) {
-  this.word = word;
-  this.guessesAllowed = guessesAllowed;
-  this.lettersArr = word.toLowerCase().split('');
-  this.guessedLetters = [];
-  this.status = 'playing';
-};
+class Hangman {
+  constructor(word, guessesAllowed) {
+    this.word = word;
+    this.guessesAllowed = guessesAllowed;
+    this.lettersArr = word.toLowerCase().split('');
+    this.guessedLetters = [];
+    this.status = 'playing';
+  }
+  
+  getPuzzle() {
+    let puzzle = '';
+    this.lettersArr.forEach((letter) => {
+      if (this.guessedLetters.includes(letter)) {
+        puzzle += letter;
+      } else if (letter === ' ') {
+        puzzle += ' ';
+      } else {
+        puzzle += '*';
+      }
+    });
+  
+    return puzzle;
+  }
 
-Hangman.prototype.getPuzzle = function() {
-  let puzzle = '';
-  this.lettersArr.forEach((letter) => {
-    if (this.guessedLetters.includes(letter)) {
-      puzzle += letter;
-    } else if (letter === ' ') {
-      puzzle += ' ';
-    } else {
-      puzzle += '*';
+  makeGuessed(guess) {
+    guess = guess.toLowerCase();
+    if (this.status === 'playing') {
+      if (!(guess >= 'A')) {return} // do nothing if it's not a chracter
+      if (this.guessedLetters.includes(guess)) {return} // do nothing if letter is already guessed
+      if (this.lettersArr.includes(guess)) {
+        this.guessedLetters.push(guess);
+      } else {
+        this.guessedLetters.push(guess);
+        this.guessesAllowed --;
+      }
+    
+      // call function that calculate the status everytime user guesses
+      this.calculateStatus();
     }
-  });
+  }
 
-  return puzzle;
-}
-
-// Ways to check if the input letter is alphabet (typeof is not going to work in this case
-// because '1' also type of "string")
-// var isAlpha = (c) => (/[A-Za-z]/).test(c);
-// character >= 'A'
-Hangman.prototype.makeGuessed = function(guess) {
-  guess = guess.toLowerCase();
-  if (this.status === 'playing') {
-    if (!(guess >= 'A')) {return} // do nothing if it's not a chracter
-    if (this.guessedLetters.includes(guess)) {return} // do nothing if letter is already guessed
-    if (this.lettersArr.includes(guess)) {
-      this.guessedLetters.push(guess);
+  createStatusMessage(){
+    if (this.status === 'playing') {
+      return `Guesses left: ${this.guessesAllowed}`;
+    } else if (this.status === 'failed') {
+      return `Nice try! The word was "${this.word}".`;
     } else {
-      this.guessedLetters.push(guess);
-      this.guessesAllowed --;
+      return 'Great work! you guessed the word.';
     }
   
-    // call function that calculate the status everytime user guesses
-    this.calculateStatus();
-  }
-}
-
-
-// Create a status message
-Hangman.prototype.createStatusMessage = function() {
-  if (this.status === 'playing') {
-    return `Guesses left: ${this.guessesAllowed}`;
-  } else if (this.status === 'failed') {
-    return `Nice try! The word was "${this.word}".`;
-  } else {
-    return 'Great work! you guessed the word.';
   }
 
-}
-
-Hangman.prototype.calculateStatus = function() {
-  let allPass = true;
-  this.lettersArr.forEach((letter) => {
-    if (!(this.guessedLetters.includes(letter))) {
-      allPass = false;
-    }
-  })
-
-  if (allPass) {
-    this.status ='finished';
-  } else {
-    if (this.guessesAllowed === 0) {
-      this.status = 'failed';
+  calculateStatus() {
+    let allPass = true;
+    this.lettersArr.forEach((letter) => {
+      if (!(this.guessedLetters.includes(letter))) {
+        allPass = false;
+      }
+    })
+  
+    if (allPass) {
+      this.status ='finished';
     } else {
-      this.status = 'playing';
+      if (this.guessesAllowed === 0) {
+        this.status = 'failed';
+      } else {
+        this.status = 'playing';
+      }
     }
   }
 
+  
 }
 
 
